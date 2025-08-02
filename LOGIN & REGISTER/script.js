dc33869079801b754cc9825d501fc299496a79ba
ANGEL ENRIQUE LUQUE UNTIVEROS <wentrap20@gmail.com>
2025-08-02 15:32:24 -0500

    Delete LOGIN & REGISTER directory

--- LOGIN & REGISTER/script.js
+++
@@ -1,9 +0,0 @@
-const wrapper = document.querySelector('.wrapper');
-
-function registerActive() {
-    wrapper.classList.toggle('active');
-}
-
-function loginActive() {
-    wrapper.classList.toggle('active');
-}
